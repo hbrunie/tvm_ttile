8e0257af4681eeb516db33781c28b7ee16ce5448
S12P <{26119556}+{S12P}@users.noreply.github.com>
2021-02-19 15:47:37 +0100

    folder tmp

---
+++ tmp/ttile.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "ttile.h"
+void ttile_conv2d(float* K, float * Input, float * Output, int W, int H, int C, int F, int X, int Y) { printf("newww functionnn");
+ for (int f=0; f<F; f++){
+   for (int y=0; y<Y; y++){
+     for (int x=0; x<X; x++){
+       Output[f*Y*X + y*X + x] = 0.0;
+       for (int c=0; c<C; c++){
+         for (int h=0; h<H; h++){
+           for (int w=0; w<W; w++){
+             Output[f*Y*X + y*X + x] += Input[c*(Y+H-1)*(X+W-1) + (y+h)*(X+W-1) + x+w] * K[f * C*H*W + c * H*W + h*W + w];
+           }
+         }
+       }
+     }
+    }
+   }
+   return;
+}
---
+++ tmp/ttile.h
@@ -0,0 +1,4 @@
+#ifndef _TTILE_H_
+#define _TTILE_H_
+extern void ttile_conv2d(float *, float *, float *, int, int, int, int, int, int);
+#endif
