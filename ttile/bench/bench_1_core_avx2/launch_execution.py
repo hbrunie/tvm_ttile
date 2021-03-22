519684810ab62043ce3b9d9b1888afe739267ac0
S12P <{26119556}+{S12P}@users.noreply.github.com>
2021-03-22 18:49:52 +0100

    launch:

--- ttile/bench/bench_1_core_avx2/launch_execution.py
+++ ttile/bench/bench_1_core_avx2/launch_execution.py
@@ -1,5 +1,22 @@
 import os
 import input_conv
 
+import time
+
 for key in input_conv.input_conv:
+    t1 = time.time()
     os.system("python3 conv_NCHW.py " + key)
+    t2 = time.time()
+    t = t2 - t1
+    f = open("temps_recherche.txt", "a")
+    f.write(str(t) + "\n")
+    f.close()
+
+for key in input_conv.input_conv:
+    t1 = time.time()
+    os.system("python3 conv_NCHW_ga.py " + key)
+    t2 = time.time()
+    t = t2 - t1
+    f = open("temps_recherche_ga.txt", "a")
+    f.write(str(t) + "\n")
+    f.close()
